bb8b863e31a191c2bfa571925c5ec38f76bab6db
Aram <arams0710@gmail.com>
2021-07-05 20:36:20 +0900

    Revert "node를 이용해서 twitter api 타임라인 조회 및 멘션하기."
    This reverts commit e046cff1efa48a9a547a849a4fde973ac3814a0d.

--- public/commonenum.js
+++
@@ -1,8 +0,0 @@
-global.nullstring = '';
-global.SUCCESS = 1;
-global.NOT_FOUND_INDEX = -1;
-global.oAuthTokenList = [];
-
-const GOOGLE_SHEET_NUMBER = {
-    TWEET_NUMBER: 1,
-}; exports.GOOGLE_SHEET_NUMBER = GOOGLE_SHEET_NUMBER;
